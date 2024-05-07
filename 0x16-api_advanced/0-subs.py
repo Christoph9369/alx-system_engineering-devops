3060880ba56a84bf63af5a71a32fcb9d810aa3d1
Christoph9369 <mojuyefelix@gmail.com>
2024-05-07 13:13:50 -0700

    2-recurse.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-function number of subscribers.
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
@@ -9,8 +9,8 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    users = {"User-Agent": "Amoh/5.0"}
-    response = requests.get(url, headers=users, allow_redirects=False)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
